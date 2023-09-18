26b1427d4681738fc63c8cc64ed8d680e5e8aa4a
Guilherme Armesto Job <guilhermearmesto@bemol.com.br>
2023-09-18 19:17:12 -0300

    Jogo de forca em C finalizado

---
+++ C/Avancando/forca.h
@@ -0,0 +1,11 @@
+#define TAMANHOPALAVRA 20
+
+
+void escolhepalavra();
+int jachutou(char letra);
+int ganhou();
+void desenhaforca ();
+void chuta();
+void abertura();
+int enforcou();
+void adicionapalavra();
